a77b81eb32eda01e7f3a3dcce907d82631538b55
borismalyavin <148041495+borismalyavin@users.noreply.github.com>
2025-01-10 11:43:06 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as di1
+from true_math import divide as di2
+
+result1 = di1(69, 3)
+result2 = di1(3, 0)
+result3 = di2(49, 7)
+result4 = di2(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
